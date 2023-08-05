6d166504901340a63ee35a1f4f26ed92d713c4ee
kyk <kykim6014@gmail.com>
2023-08-05 23:56:32 +0900

    동적으로 요소 변경 연습

--- app/src/main/java/com/cookandroid/baseapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/baseapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.cookandroid.baseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,14 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 클릭하면 작동할 메소드 여기
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        TextView tv1, tv2, tv3;
+        tv1 = (TextView) findViewById(R.id.textView1);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
+
+        tv1.setText("안녕하세요?");
+
+        tv2.setTextSize(40);
+        tv2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+
+        tv3.setText("가나다라마바사아자차카타파하");
+        tv3.setSingleLine();
+
+
+
+//        button1 = (Button) findViewById(R.id.button1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // 클릭하면 작동할 메소드 여기
+//                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
